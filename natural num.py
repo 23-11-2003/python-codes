ba65ae3e43660ddbf87cef5e122d96d4a18e623c
Srujana Mummadi <104927950+23-11-2003@users.noreply.github.com>
2024-09-10 22:14:31 +0530

    Add files via upload

---
+++ natural num.py
@@ -0,0 +1,17 @@
+'''n = int(input())
+for i in range(1,n+1):
+    print(i)'''
+    
+"""n = int(input())
+i=1
+while i<=n:
+    print(i)
+    i += 1"""
+    
+    
+#sum of n natural numbers
+n = int(input())
+sum = 0
+for i in range(1,n+1):
+    sum+=i
+print(sum)
